e14e5b9151960e96ede5b63efdc781cd997cfb54
yeom0331 <yeom0331@naver.com>
2021-03-04 13:19:34 +0900

    add file

---
+++ multi-thread-circular-buffer/main.cpp
@@ -0,0 +1,67 @@
+#include <iostream>
+#include <pthread.h>
+#define SIZE 8
+using namespace std;
+
+void *producer(void *arg);
+void *customer(void *arg);
+
+int buf[SIZE];
+int count=0, in= -1, out = -1;
+
+pthread_mutex_t mutex;
+pthread_cond_t buffer_has_space, buffer_has_data;
+
+int main(void)
+{
+    int i;
+    pthread_t threads[2];
+    pthread_create(&threads[0], NULL, producer, NULL);
+    pthread_create(&threads[1], NULL, customer, NULL);
+
+    for(i=0; i<2; i++) {
+        pthread_join(threads[i], NULL);
+    }
+
+    pthread_cond_destroy(&buffer_has_space);
+    pthread_cond_destroy(&buffer_has_data);
+
+    pthread_exit(NULL);
+}
+
+void *producer(void *arg) {
+    int i;
+    for(i=1; i<=SIZE; i++) {
+        printf("producer data : %d\n", i);
+        pthread_mutex_lock(&mutex);
+        if(count == 8) {
+            pthread_cond_wait(&buffer_has_space, &mutex);
+        }
+        in++;
+        in %= 8;
+        buf[in] = i;
+        count++;
+
+        pthread_cond_signal(&buffer_has_data);
+        pthread_mutex_unlock(&mutex);
+    }
+}
+
+void *customer(void *arg) {
+    int i, data;
+
+    for(i=1; i<=SIZE; i++) {
+        pthread_mutex_lock(&mutex);
+        if(count==0) {
+            pthread_cond_wait(&buffer_has_data, &mutex);
+        }
+        out++;
+        out %= 8;
+        data = buf[out];
+        count--;
+
+        pthread_cond_signal(&buffer_has_space);
+        pthread_mutex_unlock(&mutex);
+        printf("        consumer data=%d\n", data);
+    }
+}
